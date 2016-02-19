7e3df2f9f143c914e56b5921db2075bca13a3103
Ryan <jxdalang2015@gmail.com>
2016-02-19 22:55:36 +0800

    complete deployment report & startup shell script on Linux
    完成所有部署&完成了linux上启动脚本

--- deployment/shell/cloud-crawler.sh
+++ deployment/shell/cloud-crawler.sh
@@ -1,1 +1,45 @@
-#
+#/bin/bash
+
+# startup shell script for cloud-crawler
+# @author Ryan Z
+
+# cloud-crawler [--help]
+if [ $# -lt 1 ] || [ $1 = "--help" ]
+then
+    echo "Usage: cloud-crawler [OPTION]"
+    echo "OPTION list:"
+    echo "	--help		see usage."
+    echo "	-m, --master	strat master service."
+    echo "	-s, --slave	strat slave service."
+
+# cloud-crawler -m/--master
+elif [ $1 = "-m" ] || [ $1 = "--master" ]
+then
+    if [ -e "/usr/crawler/cloud-crawler-jar-with-dependencies.jar" ]
+    then
+        cd /usr/crawler
+        java -jar cloud-crawler-jar-with-dependencies.jar master
+        echo "[INFO] Master service started."
+    else
+        echo "[ERROR] Target file not found!"
+        echo "[INFO] Master service NOT started."
+    fi
+
+# cloud-crawler -s
+elif [ $1 = "-s" ] || [ $1 = "--slave" ]
+then
+    if [ -e "/usr/crawler/cloud-crawler-jar-with-dependencies.jar" ]
+    then
+        cd /usr/crawler
+        java -jar cloud-crawler-jar-with-dependencies.jar slave
+        echo "[INFO] Slave service started."
+    else
+        echo "[ERROR] Target file not found!"
+        echo "[INFO] Slave service NOT started."
+    fi
+
+# other
+else
+    echo "[ERROR] Wrong argument!"
+fi
+
