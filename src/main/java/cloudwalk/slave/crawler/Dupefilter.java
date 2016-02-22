2ea9be15eb5fa34c39a89ad4765ce0718a81c59b
TJZ1990 <tjz2009@yeah.net>
2016-02-23 00:33:32 +0800

    fix dupefilter bug

--- src/main/java/cloudwalk/slave/crawler/Dupefilter.java
+++ src/main/java/cloudwalk/slave/crawler/Dupefilter.java
@@ -1,4 +1,5 @@
 package cloudwalk.slave.crawler;
+
 import edu.uci.ics.crawler4j.url.URLCanonicalizer;
 import edu.uci.ics.crawler4j.url.WebURL;
 import redis.clients.jedis.Jedis;
@@ -8,24 +9,28 @@ import uk.org.lidalia.slf4jext.LoggerFactory;
 public class Dupefilter {
     private static final Logger logger = LoggerFactory
             .getLogger(Dupefilter.class);
-    
+
     Jedis server;
     String dupefilterKey;
-    
+
     public Dupefilter(Jedis server, String dupefilterKey) {
         this.server = server;
         this.dupefilterKey = dupefilterKey;
     }
-    
+
     public boolean seenURL(WebURL url) {
+        if (url.getURL() == null)
+            return true;
         String canonicalUrl = URLCanonicalizer.getCanonicalURL(url.getURL());
+        if (canonicalUrl == null)
+            return true;
         Long added = server.sadd(dupefilterKey, canonicalUrl);
-        if(added == 0) {
+        if (added == 0) {
             logger.trace("{} is seen before", canonicalUrl);
         }
         return added == 0;
     }
-    
+
     public void clear() {
         server.del(dupefilterKey);
     }
